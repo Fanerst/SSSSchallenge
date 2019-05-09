4c05772a00cab430cae68e354a30bcd385a527e7
Feng Pan <fan_physics@126.com>
2019-05-09 18:46:37 +0800

    change to float64

--- src/exact.py
+++ src/exact.py
@@ -6,7 +6,7 @@ from utils import readgraph, sum_up_tree, energy_ising
 
 
 def exact_config(D):
-    config = np.zeros([2**D, D], dtype=np.float32)
+    config = np.zeros([2**D, D], dtype=np.float64)
     for i in range(2**D-1, -1, -1):
         num = i
         for j in range(D-1, -1, -1):
@@ -19,7 +19,7 @@ def exact_config(D):
 def exact_logZ(D, beta, device):
     num_edges, edges, _, J = readgraph(D)
 
-    J = torch.from_numpy(J).to(device)
+    J = torch.from_numpy(J).double().to(device)
 
     G = nx.Graph()
     G.add_nodes_from(np.arange(D))
@@ -39,7 +39,7 @@ def exact_logZ(D, beta, device):
 
     l = len(FVS)
     start_time = time.time()
-    sample = torch.from_numpy(exact_config(l) * 2.0 - 1.0).to(device)
+    sample = torch.from_numpy(exact_config(l) * 2.0 - 1.0).double().to(device)
     fe_tree = sum_up_tree(sample, J, FVS, tree1, tree_hierarchy,
                           sample.shape[0], beta, device)
     energy = energy_ising(sample, J[FVS][:, FVS], l) + fe_tree
@@ -53,6 +53,6 @@ if __name__ == '__main__':
     D = 60
     beta = 1
     device = 'cpu'
-    logZ, times = exact_Z(D, beta, device)
+    logZ, times = exact_logZ(D, beta, device)
     print(logZ, times)
 
